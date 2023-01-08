d02755a7a8436b25f7dcbb52ad22c5031eca87de
T1To48 <tofek.98@gmail.com>
2023-01-08 15:11:33 +0200

    #

---
+++ 1.1-1.2/1.1_primitive_data_types.js
@@ -0,0 +1,17 @@
+//1
+let str1ng="i'm S.T.R.I.N.G!";
+//2
+let numb=4;
+//3
+let pc={CPU:'i7', RAM:'16Gb DDr5',OS:'Win 11' };
+//4
+let bool=true;
+let bool1=false;
+//5
+let noll=null;
+//6
+let und;
+//7
+let symbl = Symbol("COOL");
+
+console.log(typeof(str1ng),typeof(numb),typeof(pc) ,typeof(bool),typeof(bool1),typeof(noll),typeof(und),typeof(symbl));
---
+++ 1.2_more_on_primitive_data_types.js
@@ -0,0 +1,60 @@
+Front-end development:
+Basics
+JavaScript – primitive data types
+The following exercise contains the following subjects:
+● primitive data types
+Instructions
+Copy the code below to your computer, answer the
+questions on the file and submit.
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+d) -4
+3. Which of the following is/are booleans?
+a) true
+b) false
+4. What is the result of 80 + 71.2?
+a) 151.2
+5. What is the result of "80" + 71.2?
+c) "8071.2"
+6. Does 100 + 30 produce a number or a string?
+a) number
+7. Does "100" + 30 produce a number or a string?
+b)string
+*/
+//------Submit your answers under the
+question------//
+//create three different ways to declare variables:
+Let exmpl;
+var exmpl;
+const exmpl;
+//declare a variable and reassign a value to it:
+let x=5;//x=5
+x+=5;//x=10
+x="Apple"// string "Apple"
+//create a variable and after that assign a string
+with its value being: He's got it!
+let=4444;
+let="4444";
+/*
+1. create a variable and assign a value on how
+much a restaurant bill is:
+let price=14.99;
+Total to be paid (service is included ), $${price}.
+
+2. create a variable and assign a value on how
+much tax they should pay:
+let tax=18%;
+total Taxes to be paid this month:$${tax*price}.
+3. create a variable that will calculate the bill
+* tax and its output would be: Your total bill is
+<total bill> $.
+total to be Paid:$${(tax*price)+price}
+*/
+// Round the number 50.6 to its nearest integer
+Math.around(50.6);
+//Create a variable that is undefined:
+let undef;
