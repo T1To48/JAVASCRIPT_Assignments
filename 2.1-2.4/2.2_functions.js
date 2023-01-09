cfb1d51abebc152206dd3abc971ed5326e9e0af6
T1To48 <tofek.98@gmail.com>
2023-01-09 08:21:48 +0200

    #

--- 2.1-2.4/2.2_functions.js
+++ 2.1-2.4/2.2_functions.js
@@ -1,11 +1,11 @@
-function tellFortune(jobTitle,location,partnerName,numberOfChildren){
+function tellFortune(){
    let arbeit=['Programmer','welder','doctor','Police Officer'];
    let ort=['Manhatten','Pheonix arizona','Copenhagen','Berlin'];
    let part=['Cloey','Hannah','Layla','Sara']
    let kind=['2 children','3 children','5 children','no children'];
-   jobTitle = arbeit[Math.floor(Math.random()*4)];
-   location = ort[Math.floor(Math.random()*4)];
-   partnerName = part[Math.floor(Math.random()*4)];
+   let jobTitle = arbeit[Math.floor(Math.random()*4)];
+   let location = ort[Math.floor(Math.random()*4)];
+   let partnerName = part[Math.floor(Math.random()*4)];
    numberOfChildren = kind[Math.floor(Math.random()*4)];
    return `you will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren}.`
 }
