61699e42000788595d9316ce54bf3a65caf23d56
T1To48 <tofek.98@gmail.com>
2023-01-16 00:54:13 +0200

    #

--- 10,1/10.1_callback_functions.js
+++ 10,1/10.1_callback_functions.js
@@ -1,1 +1,12 @@
-aaaaaaaaaa
+//////////////***1***///////////////
+function cllbck(){
+   console.log(x)
+}
+function isString(str){
+   if(typeof str ==="string"){
+      return cllbck();
+   }
+}
+let x='1dfgdfgdfgdgdfg';
+isString(x);
+//////////////***2***///////////////
