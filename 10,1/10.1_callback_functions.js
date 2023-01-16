c01890671cde716049e185da1dc6502f1e431a08
T1To48 <tofek.98@gmail.com>
2023-01-16 08:53:18 +0200

    #

--- 10,1/10.1_callback_functions.js
+++ 10,1/10.1_callback_functions.js
@@ -26,3 +26,42 @@ return callbck(arr);
 }
 
 toUpperCase("my name is tawfiq im 24 years old");
+//////////////***3***///////////////
+function callbck(string){
+
+let finalStr=string.replace(/,/g,"$$");
+console.log(finalStr);
+}
+function toUpperCase(str){
+let arr=str.split(" ");
+let firstWordCapital=arr[0].toUpperCase();
+arr.shift();
+arr.unshift(firstWordCapital);
+let editedStr=arr.toString();
+return callbck(editedStr);
+
+
+}
+
+toUpperCase("my name is tawfiq im 24 years old");
+//////////////***4***///////////////
+// a function that finds all the 't' 
+//letters in a string and console log how much if finds
+function cllbckfunc(show){
+   console.log(show);
+}
+function tFinder(strng){
+   let arr=strng.split(" ");
+   for(let x=0;x<arr.length;x++){
+      let aot=arr[x].split('');
+      for(let i=0;i<arr.length;i++){
+         let letter=aot[i].toLowerCase()
+         if(letter==="t"){
+            return cllbckfunc(arr[x][i])
+         }
+      }
+      
+   }
+}
+tFinder("my name is the tawfiq")
+
